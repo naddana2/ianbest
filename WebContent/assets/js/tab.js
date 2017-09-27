1982b5c37f8b33faf8bb3afb18166ad653d48ea5
Young <Young@young>
2017-09-27 16:39:35 +0900

    다시

---
+++ WebContent/assets/js/tab.js
@@ -0,0 +1,11 @@
+
+	jQuery(document).ready(function($){
+		
+		
+
+		$('.nav-tabs > .li-menu > a').hover(function() {
+			  $(this).tab('show');
+			  $(this).css('transition','2.25s');
+			});
+	});
+	
