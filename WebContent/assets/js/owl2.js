532834255e9aeff8ca29a96b46d05ec8cf7546cb
Young <Young@young>
2017-10-17 16:47:44 +0900

    index about 모바일完

--- WebContent/assets/js/owl2.js
+++ WebContent/assets/js/owl2.js
@@ -4,9 +4,9 @@
 
 $(document).ready(function() {
 
-	 $("#owl-demo").owlCarousel({
+	/* var owl = $("#owl-demo").owlCarousel({
 		 
-	      navigation : true, // Show next and prev buttons
+	      navigation : false, // Show next and prev buttons
 	 
 	      slideSpeed : 300,
 	      paginationSpeed : 400,
@@ -16,8 +16,49 @@ $(document).ready(function() {
 	      itemsDesktopSmall : false,
 	      itemsTablet: false,
 	      itemsMobile : false
-	 
-	  });
+	      
+	  });*/
+
+	var owl = $('.small-owl').owlCarousel({
+			center: true,
+			items: 3,
+			loop: true,
+			margin: 10,
+			nav: false,
+			
+			 responsive: {
+		         0: {
+		            items: 1
+		           
+		         },
+		         375: {
+		            items: 2
+		            
+		         },
+		         530: {
+		            items: 3
+		         }
+		      }
+			
+		
+		});
+		
+		
+		
+		
+	// Go to the next item
+	$('.owl-prev').click(function() {
+	    owl.trigger('next.owl.carousel');
+	})
+	// Go to the previous item
+	$('.owl-next').click(function() {
+	    // With optional speed parameter
+	    // Parameters has to be in square bracket '[]'
+	    owl.trigger('prev.owl.carousel', [300]);
+	})
+	
+	
 
 });
+
 
