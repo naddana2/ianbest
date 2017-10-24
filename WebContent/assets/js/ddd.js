ad9000708fff2afa1ea2bd12c63e2f9d648a33af
JongChan <JongChan@Jong>
2017-10-24 18:10:14 +0900

    ddd.js

--- WebContent/assets/js/ddd.js
+++ WebContent/assets/js/ddd.js
@@ -9,91 +9,85 @@ item += '<div class="iso-box remodeling"><div class="portfolio-thumb" onclick="l
 item += '<div class="iso-box office"><div class="portfolio-thumb" onclick="location.href=\'portfolio_detail.html\'" style="cursor: pointer;"><img class="" src="assets/images/portfolio-11.png" alt="GTA V"><div class="thumb-text"><p>Portfolio, office</p><h2>K병원</h2></div></div></div>';
 
 
-	jQuery(window).load(function() {
 
-		var owl = $('.small-owl').owlCarousel({
-			items: 1,
-
-			nav: false,
-
-		});
-		// Go to the next item
-		$('.owl-prev').click(function() {
-			owl.trigger('prev.owl.carousel', [300]);
-		})
-		// Go to the previous item
-		$('.owl-next').click(function() {
-			// With optional speed parameter
-			// Parameters has to be in square bracket '[]'
-			owl.trigger('next.owl.carousel', [300]);
-		})
-
+jQuery(window).load(function() {
+	
+	$('#project').addClass('selected');
+	
+	setTimeout(() => {
+		$isop.isotope('layout');
+	}, 100);
+	
+	var $isop =  $('.masonry').isotope({
+		masonry : {
+			itemSelector : 'iso-box',
+			columnWidth: '.grid-sizer',
+			horizontalOrder : true,
+		}
 	});
 	
-
-
-	$(document).ready(function() {
-
-		$('#project').addClass('selected');
-
-		var $isop = $('.masonry').isotope({
-			masonry : {
-				itemSelector : 'iso-box',
-				columnWidth: '.grid-sizer',
-				horizontalOrder : true,
-			}
-		});
-
-		// filter functions
-		var filterFns = {
-				// show if number is greater than 50
-				numberGreaterThan50 : function() {
-					var number = $(this).find('.number').text();
-					return parseInt(number, 10) > 50;
-				}
-		};
-
-		// bind filter button click
-		$('#filters').on('click', 'a', function() {
-			var filterValue = $(this).attr('data-filter');
-			// use filterFn if matches value
-			filterValue = filterFns[filterValue] || filterValue;
-			$isop.isotope({
-				filter : filterValue
-			});
-		});
-
-		// change is-checked class on buttons
-		$('.button-group').each(function(i, buttonGroup) {
-			var $buttonGroup = $(buttonGroup);
-			$buttonGroup.on('click', 'a', function() {
-				$buttonGroup.find('.is-checked').removeClass('is-checked');
-				$(this).addClass('is-checked');
-			});
-		});
-		var $items = $(item);
-		var flag = true;
-		var icon = document.getElementById('icon');
-
-		$('.append-button').on('click', function() {
-			// create new item elements
-			if (flag == true) {
-				$isop.append($items).isotope('appended', $items);
-				setTimeout(function() {
-					icon.className = "fa fa-chevron-up gray";
-				}, 250);
-				flag = false;
-			} else {
-				$isop.isotope('remove', $items);
-				setTimeout(function() {
-					$isop.isotope('layout');
-					icon.className = "fa fa-chevron-down gray";
-				}, 250);
-				flag = true;
-			}
-		});
-
+	var owl = $('.small-owl').owlCarousel({
+		items: 1,
+		nav: false,
 	});
+
+
+	// filter functions
+	var filterFns = {
+			// show if number is greater than 50
+			numberGreaterThan50 : function() {
+				var number = $(this).find('.number').text();
+				return parseInt(number, 10) > 50;
+			}
+	};
+
+	// bind filter button click
+	$('#filters').on('click', 'a', function() {
+		var filterValue = $(this).attr('data-filter');
+		// use filterFn if matches value
+		filterValue = filterFns[filterValue] || filterValue;
+		$isop.isotope({
+			filter : filterValue
+		});
+	});
+
+	// change is-checked class on buttons
+	$('.button-group').each(function(i, buttonGroup) {
+		var $buttonGroup = $(buttonGroup);
+		$buttonGroup.on('click', 'a', function() {
+			$buttonGroup.find('.is-checked').removeClass('is-checked');
+			$(this).addClass('is-checked');
+		});
+	});
+	var $items = $(item);
+	var flag = true;
+	var icon = document.getElementById('icon');
+
+	$('.append-button').on('click', function() {
+		// create new item elements
+		if (flag == true) {
+			$isop.append($items).isotope('appended', $items);
+			setTimeout(function() {
+				icon.className = "fa fa-chevron-up gray";
+			}, 250);
+			flag = false;
+		} else {
+			$isop.isotope('remove', $items);
+			setTimeout(function() {
+				$isop.isotope('layout');
+				icon.className = "fa fa-chevron-down gray";
+			}, 250);
+			flag = true;
+		}
+	});
+	
+	
+
+});
+
+
+
+	
 
 
 
